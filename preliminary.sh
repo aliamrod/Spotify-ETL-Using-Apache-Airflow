43742a3e725a0bb2c4f7adbaae0bf2c8e31498c3
Alia Mahama-Rodríguez <62684338+aliamrod@users.noreply.github.com>
2024-05-30 17:25:52 -0500

    Update preliminary.sh

--- preliminary.sh
+++ preliminary.sh
@@ -6,7 +6,16 @@ $ source airflow-venv/bin/activate # activate virtual environment
 
 # Set Airflow `home` variable. The AIRFLOW_HOME environment variable is used to inform Airflow of the desired project location. This step of setting
 # ... the environment variable is performed before installing Airflow so that the installation process knows where to store the necessary files. 
-$ export AIRFLOW_HOME=/Users/aliamahama-rodriguez/spotify-airflow 
+$ export AIRFLOW_HOME=/Users/aliamahama-rodriguez/spotify-airflow # direct `AIRFLOW_HOME` variable to desired project directory
 
-# Install Airflow using the constraints file, which is determined based on the URL we passed
-AIRFLOW_VERSION = 
+$ pip install apache-airflow 
+
+# Edit .cfg/configuration file
+$ nano *.cfg
+# dag_file = /Users/aliamahama-rodriguez/spotify-airflow
+
+$ sudo lsof -i :8080
+sudo kill -9 <PID> # Replace <PID> with the process ID using port 8080 (Kill any other active webserver ports)
+
+# Initiate webserver port
+$ airflow webserver --port 8081
